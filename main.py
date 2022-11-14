ad0ccd58c7cc9854f30d0df7a90af7a80fa0bf23
edge2g <maira.cristian54@gmail.com>
2022-11-14 01:40:39 -0300

    smoother turn curves

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ class Curve:
     def __init__(self):
         self.x_pos = 800
         self.y_pos = 450
+        self.prev_x = self.x_pos
+        self.prev_y = self.y_pos
         self.head = pg.image.load("graphics/head.png")
-        self.trail = []
-        #self.trail = pg.image.load("graphics/trail_up.png")
+        self.invis_trail = pg.image.load("graphics/invis_trail.png")
+        self.trail_pos = []
+        self.invis_trail_pos = []
+        self.trail_px_delay = 8
         self.direction = randint(0, 15)
-        self.speed = 3
+        self.speed = 2
 
     def inc_direction(self):
         if self.direction == 15:
@@ -102,9 +106,13 @@ def main():
     game_screen = pg.display.set_mode((W_WIDTH, W_HEIGHT))
     pg.display.set_caption(W_TITLE)
     clock = pg.time.Clock()
+    timer_event = pg.USEREVENT+1
+    erase_event = pg.USEREVENT+2
+    time_delay = 3000
+    pg.time.set_timer(timer_event, time_delay)
 
     background_surface = pg.Surface((W_WIDTH, W_HEIGHT))
-    background_surface.fill((17, 22, 48))
+    background_surface.fill((0, 0, 0))
     game_area = pg.Rect(W_WIDTH//3, 10, int(0.66*W_WIDTH), int(0.98*W_HEIGHT))
     pg.draw.rect(background_surface, (100, 100, 100), game_area, 8, 3)
     game_screen.blit(background_surface, (0, 0))
@@ -112,61 +120,72 @@ def main():
     player1 = Curve()
     isPressed_rkey = False
     isPressed_lkey = False
+    isDrawing = True
+    trail_frame_counter = 0
+    frame_delay = 5
 
     while(True):
+        
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 exit()
 
+            if event.type == timer_event:
+                next_invis_trail = randint(1, 4)
+                time_delay = next_invis_trail * 1000
+                pg.time.set_timer(timer_event, time_delay)
+                
+                if isDrawing:
+                    isDrawing = False
+                    invis_trail_duration = randint(FPS*500, FPS*1000)
+                else:
+                    isDrawing = True
+
             if event.type == pg.KEYDOWN:
-                '''
-                if event.key == pg.K_UP:
-                    player1.direction = "up"
-
-                if event.key == pg.K_DOWN:
-                    player1.direction = "down"
-                '''
-
                 if event.key == pg.K_LEFT:
                     isPressed_lkey = True
-                    #player1.dec_direction()
-                    #print(player1.direction)
-                    #player1.direction = "left"
 
                 if event.key == pg.K_RIGHT:
                     isPressed_rkey = True
-                    #player1.inc_direction()
-                    #print(player1.direction)
-                    #player1.direction = "right"
 
                 if event.key == pg.K_SPACE:
                     player1.speed += 1
 
             if event.type == pg.KEYUP:
-
                 if event.key == pg.K_LEFT:
                     isPressed_lkey = False
                 if event.key == pg.K_RIGHT:
                     isPressed_rkey = False
 
+
         game_screen.blit(player1.head, (player1.x_pos, player1.y_pos))
 
-        '''
-        if player1.direction == "up":
-            player1.y_pos -= player1.speed
-        if player1.direction == "down":
-            player1.y_pos += player1.speed
-        if player1.direction == "left":
-            player1.x_pos -= player1.speed
-        if player1.direction == "right":
-            player1.x_pos += player1.speed
-        '''
+        if isDrawing:
+            player1.invis_trail_pos = []
+            trail_frame_counter = 0
+            invis_trail_idx = 0
+            player1.trail_pos.append((player1.x_pos, player1.y_pos))
+        else:
+            trail_frame_counter += 1
+            player1.invis_trail_pos.append((player1.x_pos, player1.y_pos))
+
+            if trail_frame_counter >= player1.trail_px_delay:
+                erase_coords = player1.invis_trail_pos[invis_trail_idx]
+                game_screen.blit(player1.invis_trail, (erase_coords[0], erase_coords[1]))
+                invis_trail_idx += 1        
 
         if(isPressed_lkey):
-            player1.dec_direction()
+            frame_delay -= 1
+            if frame_delay == 0:
+                frame_delay = 5
+                player1.dec_direction()
+
         if(isPressed_rkey):
-            player1.inc_direction()
+            frame_delay -= 1
+            if frame_delay == 0:
+                frame_delay = 5
+                player1.inc_direction()
             
         player1.move()
 
